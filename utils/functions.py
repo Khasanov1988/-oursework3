47812b57a2c72089d2ec8604a8fee474b0bf5357
Ilnar Khasanov <Khasanov1988@gmail.com>
2023-03-28 23:36:32 +0200

    Add function: print_report_item and main code

--- utils/functions.py
+++ utils/functions.py
@@ -4,7 +4,7 @@ import os
 from operator import itemgetter
 
 
-def load_json(filename):
+def load_json(filename: str):
     """
     Функция загружает исходный файл с данными и конвертирует в список
     :param filename: имя файла в формате json
@@ -19,7 +19,7 @@ def load_json(filename):
         return None
 
 
-def executed_transactions(transactions):
+def executed_transactions(transactions: list[dict]):
     """
     Функция оставляет только успешные транзакции.
     :param transactions: Исходный список транзакций
@@ -28,17 +28,42 @@ def executed_transactions(transactions):
     answer = []
     for transaction in transactions:
         if not transaction:
-            print("В исходных данных есть пустой словарь")
+            print("<<<В исходных данных есть пустой словарь>>>\n")
         else:
             if transaction['state'] == "EXECUTED":
                 answer.append(transaction)
     return answer
 
 
-def sort_by_date(transactions):
+def sort_by_date(transactions: list[dict]):
     """
     Функция конвертирует и добавляет в исходный список транзакций время с эпохи
     :param transactions: исходный список транзакций
     :return: список транзакций с добавленным ключом "time" для времени с эпохи
     """
     return sorted(transactions, key=lambda row: row['date'], reverse=True)
+
+
+def print_report_item(item: dict[str, any]):
+    """
+    Функция выводит в консоль данные о транзакциях из словаря в требуемом формате
+    :param item: исходный словарь
+    :return: None
+    """
+    # Получаем дату и время транзакции из словаря
+    transaction_date = datetime.datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%S.%f')
+
+    # Получаем данные отправителя и получателя транзакции
+    if 'from' in item:
+        sender = item['from'].split()
+        sender[1] = f"{sender[1][:4]} {sender[1][4:6]}** **** {sender[1][-4:]}"
+    else:
+        sender = ['<ОТКРЫТИЕ', 'ВКЛАДА>']
+
+    recipient = item.get('to', '<НЕИЗВЕСТНАЯ_ПЛАТЕЖНАЯ_СИСТЕМА> <СЧЕТ>').split()
+    recipient[1] = recipient[1][-4:]
+
+    # Форматируем и выводим данные транзакции в консоль
+    print(f"{transaction_date:%d.%m.%Y} {item['description']}\n"
+          f"{sender[0]} {sender[1]} -> {recipient[0]} **{recipient[1]}\n"
+          f"{item['operationAmount']['amount']} {item['operationAmount']['currency']['name']}\n")
--- utils/main.py
+++ utils/main.py
@@ -1,10 +1,14 @@
-from functions import load_json, sort_by_date, executed_transactions
+from functions import load_json, sort_by_date, executed_transactions, print_report_item
 
+
+# Загружаем файл со всеми операциями в формате json
 transactions = load_json('operations.json')
-print(transactions)
+# Оставляем только успешные транзакции
 ex_transactions = executed_transactions(transactions)
-print(ex_transactions)
+# Сортируем транзакции от более ранних к более поздним
 sorted_transactions = sort_by_date(ex_transactions)
-print(sorted_transactions)
+# Выводим информацию по последним 5 транзакциям
+for i in range(min(5, len(sorted_transactions))):
+    print_report_item(sorted_transactions[i])
 
 
